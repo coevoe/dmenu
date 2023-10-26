97378275df20f4edf9816d678d1ddf3d3a2c2605
coevoe <grastlinka@gmail.com>
2023-10-26 17:15:53 +0200

    Correted text formatting

--- config.def.h
+++ config.def.h
@@ -2,15 +2,11 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
-static int incremental =
-    0; /* -r  option; if 1, outputs text each time a key is pressed */
+static int incremental = 0; /* -r  option; if 1, outputs text each time a key is pressed */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const int user_bh =
-    2; /* add an defined amount of pixels to the bar height */
-
+static const int user_bh = 2; /* add an defined amount of pixels to the bar height */
 static const char *fonts[] = {"JetBrainsMono Nerd Font:size=9"};
-static const char *prompt =
-    NULL; /* -p  option; prompt to the left of input field */
+static const char *prompt = NULL; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
     /*     fg         bg       */
     [SchemeNorm] = {"#abb2bf", "#282c34"},
@@ -22,7 +18,6 @@ static const char *colors[SchemeLast][2] = {
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines = 0;
-
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
